d1c972734c15abff4eb09792603df50c93580756
Alejandro A. Cristóbal <acristobalgarces@hotmail.com>
2023-12-21 13:53:42 +0100

    Add file fxml

--- src/main/java/com/svalero/editor/App.java
+++ src/main/java/com/svalero/editor/App.java
@@ -1,11 +1,29 @@
 package com.svalero.editor;
-
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class App extends Application {
+    @Override
+    public void init() throws Exception {
+        // TODO Borrar mensaje.
+        System.out.println("Starting JavaFX Appplication");
+        super.init();
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("main.fxml"));
+        //loader.setController(new AppController());
+        Scene scene = new Scene(loader.load());
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        throw new UnsupportedOperationException("Unimplemented ");
+    }
+
+    public static void main(String[] args) {
+        launch();
     }
 }
